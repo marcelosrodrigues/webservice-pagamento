76591d64f2bf485063f41bda4af6d18e32c41a5c
Marcelo Rodrigues <marcelosrodrigues@globo.com>
2014-09-21 15:58:28 -0300

    alterando a construção do banco de dados

--- src/main/java/com/pmrodrigues/webservices/PagamentoWS.java
+++ src/main/java/com/pmrodrigues/webservices/PagamentoWS.java
@@ -12,7 +12,11 @@ import javax.jws.soap.SOAPBinding;
 import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Marceloo on 19/09/2014.
@@ -22,7 +26,7 @@ import java.util.Date;
 public class PagamentoWS implements Serializable {
 
     @WebMethod(action = "enviar", operationName = "enviar")
-    public String enviarBoleto(@WebParam(name = "DataVencimento") Date dataVencimento, @WebParam(name = "DataEmissao")Date dataEmissao,
+    public String enviarBoleto(@WebParam(name = "DataVencimento") String dataVencimento, @WebParam(name = "DataEmissao")String dataEmissao,
                                @WebParam(name = "NumeroAgencia")Integer numeroAgencia, @WebParam(name = "DigitoAgencia")char digitoAgencia,
                                @WebParam(name = "NumeroContaCorrente") Long numeroContaCorrente, @WebParam(name = "DigitoContaCorrente")char digitoContaCorrente,
                                @WebParam(name = "NumeroBoleto")String numeroBoleto,@WebParam(name = "NossoNumero") Long nossoNumero,
@@ -31,15 +35,20 @@ public class PagamentoWS implements Serializable {
                                @WebParam(name = "EnderecoSacado") String enderecoSacado, @WebParam(name = "BairroSacado") String bairroSacado,
                                @WebParam(name = "CEPSacado") String cepSacado, @WebParam(name = "CidadeSacado") String cidadeSacado,
                                @WebParam(name = "EstadoSacado") String estadoSacado, @WebParam(name = "EmailSacado")String emailSacado,
-                               @WebParam(name = "ValorBoleto") BigDecimal valorBoleto, @WebParam(name = "Carteira") Integer carteira) throws IOException, EmailException {
+                               @WebParam(name = "ValorBoleto") String valorBoleto, @WebParam(name = "Carteira") Integer carteira) throws Exception {
+
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd",new Locale("pt","BR"));
+        DecimalFormat decimalFormat = (DecimalFormat) DecimalFormat.getInstance(new Locale("pt","BR"));
+        decimalFormat.applyPattern("#,##");
 
         PagamentoService service = new PagamentoService();
-        OrdemPagamento ordemPagamento = new OrdemPagamento(dataVencimento, dataEmissao,
+        OrdemPagamento ordemPagamento = new OrdemPagamento(dateFormat.parse(dataVencimento), dateFormat.parse(dataEmissao),
                                                             new Agencia(numeroAgencia,digitoAgencia),
                                                             new ContaCorrente(numeroContaCorrente,digitoContaCorrente), numeroBoleto,
                                                             new NossoNumero(nossoNumero,digitoNossoNumero),new Cedente(nomeEmissor),
                                                             new Pagador(sacadoCPF,nomeSacado ,new Endereco(enderecoSacado,bairroSacado,cepSacado,cidadeSacado,estadoSacado),emailSacado),
-                                                            valorBoleto, carteira);
+                                                            new BigDecimal(decimalFormat.parse(valorBoleto).doubleValue()), carteira);
 
         ordemPagamento = service.enviarBoleto(ordemPagamento);
 
