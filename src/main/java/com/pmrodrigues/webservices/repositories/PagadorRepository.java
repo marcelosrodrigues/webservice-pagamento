a092cb59129a6281d6e379d85c62134479a7f07d
Marcelo Rodrigues <marcelosrodrigues@globo.com>
2014-09-21 16:02:20 -0300

    alterando a construção do banco de dados

--- src/main/java/com/pmrodrigues/webservices/repositories/PagadorRepository.java
+++ src/main/java/com/pmrodrigues/webservices/repositories/PagadorRepository.java
@@ -3,6 +3,7 @@ package com.pmrodrigues.webservices.repositories;
 import com.pmrodrigues.webservices.models.Pagador;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 /**
  * Created by Marceloo on 21/09/2014.
@@ -11,10 +12,14 @@ public class PagadorRepository extends AbstractRepository<Pagador> {
 
     public Pagador getPagadorByCPF( String cpf ) {
 
-        EntityManager em = PagadorRepository.getEntityManager();
-        return em.createQuery("FROM Pagador where cpf = :cpf",Pagador.class)
-                    .setParameter("cpf",cpf)
+        try {
+            EntityManager em = PagadorRepository.getEntityManager();
+            return em.createQuery("FROM Pagador where cpf = :cpf", Pagador.class)
+                    .setParameter("cpf", cpf)
                     .getSingleResult();
+        }catch(NoResultException e){
+            return null;
+        }
 
 
     }
